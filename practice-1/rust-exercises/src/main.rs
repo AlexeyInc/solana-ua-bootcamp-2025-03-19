f39e568d554d37ea5dc3086b460572ccc80b6412
Alex Ovsianicoff <o.ovsiannykov@rozetkapay.com>
2025-04-10 16:40:16 +0300

    Added generate public/private keys in Rust script

---
+++ practice-1/rust-exercises/src/main.rs
@@ -0,0 +1,15 @@
+use solana_sdk::signature::{Keypair, Signer};
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let keypair = Keypair::new();
+    let keypair_bytes = keypair.to_bytes();
+    let serialized = serde_json::to_string(&keypair_bytes.to_vec()).unwrap();
+
+    let mut file = File::create("keypair.json").expect("Failed to create file");
+    file.write_all(serialized.as_bytes()).expect("Failed to write");
+
+    println!("✅ Generated keypair!");
+    println!("Public Key: {}", keypair.pubkey());
+}
